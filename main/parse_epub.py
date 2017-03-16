6e7defe38f9ccef28e46cf775d390594de578bdc
KrzyHonk <honkiszkrzysztof@gmail.com>
2017-03-16 22:49:12 +0100

    Added words filtering

--- main/parse_epub.py
+++ main/parse_epub.py
@@ -1,10 +1,15 @@
-from ebooklib import epub
-import re
 import html
-import spacy
+import re
+from nltk.corpus import wordnet
+import scipy.stats as stats
 
-def example():
+import spacy
+from ebooklib import epub
+
+
+def get_word_count():
     book = epub.read_epub('hitchhiker.epub')
+    words_list = []
     for document in book.get_items_of_type(9):
         content = document.get_content().decode("utf-8")
         tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
@@ -13,15 +18,29 @@ def example():
         # Remove well-formed tags, fixing mistakes by legitimate users
         no_tags = tag_re.sub(' ', content)
         # Clean up anything else by escaping
-        preprocessed_content = html.escape(no_tags).replace("\n"," ").strip()
-        print(preprocessed_content)
+        preprocessed_content = html.escape(no_tags).replace("\n", " ").strip()
+        preprocessed_content = re.sub("\s\s+", " ", preprocessed_content)
 
-        nlp = spacy.load('en')
-        doc = nlp(preprocessed_content)
-        for word in doc:
-            print(word.text, word.pos_)
-        reuters_words = [w.text.lower() for w in doc]
-        words = set(reuters_words)
-        counts = [(w, reuters_words.count(w)) for w in words]
-        print(counts)
-example()
+        if preprocessed_content:
+            # print("CONTENT: ", preprocessed_content)
+            nlp = spacy.load('en')
+            doc = nlp(preprocessed_content)
+            # [utils.to_nltk_tree(sentence.root).pretty_print() for sentence in doc.sents]
+            for word in doc:
+                if word.pos_ not in ["PUNCT", "SYM", "X", "DET", "PART", "SPACE", "INTJ"]:
+                    words_list.append(word.text.lower())
+    words_set = set(words_list)
+    words_count = [(w, words_list.count(w)) for w in words_set]
+    return words_count
+
+
+def wordnet_stats(words_count):
+    amb = [(w, c, len(wordnet.synsets(w))) for (w, c) in words_count]
+
+    amb_p_rank = stats.rankdata([p for (w, c, p) in amb])
+    amb_c_rank = stats.rankdata([c for (w, c, p) in amb])
+
+    amb_ranked = zip(amb, amb_p_rank, amb_c_rank)
+
+
+wordnet_stats(get_word_count())
