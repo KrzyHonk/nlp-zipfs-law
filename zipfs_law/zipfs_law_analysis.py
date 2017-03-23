e7c7c21edd42dd4c41a3824bfe870acd0b3c1a43
KrzyHonk <honkiszkrzysztof@gmail.com>
2017-03-23 22:20:36 +0100

    Fixed most common words retrieving

--- zipfs_law/zipfs_law_analysis.py
+++ zipfs_law/zipfs_law_analysis.py
@@ -21,29 +21,31 @@ def zipfs_law_analysis(epub_title: str):
     pairs_ranked_data = __get_pairs_count_rank(pairs_count)
     triplets_ranked_data = __get_triplets_count_rank(triplets_count)
 
-    # TODO needs fixing
-    #words_data = zip(words_count, words_ranked_data)
-    #words_most_common = __get_top_range(words_data)
+    words_data = zip(words_count, words_ranked_data)
+    words_most_common = __get_top_range(words_data)
 
-    #pairs_data = zip(pairs_count, pairs_ranked_data)
-    #pairs_most_common = __get_top_range(pairs_data)
+    pairs_data = zip(pairs_count, pairs_ranked_data)
+    pairs_most_common = __get_top_range(pairs_data)
 
-    #triplets_data = zip(triplets_count, triplets_ranked_data)
-    #triplets_most_common = __get_top_range(triplets_data)
+    triplets_data = zip(triplets_count, triplets_ranked_data)
+    triplets_most_common = __get_top_range(triplets_data)
 
     # TODO Ranked data is not serializable to JSON. Find some solution
     json_output = {
         'name': epub_title,
         'words_count': words_count,
+        'words_most_common': words_most_common,
         'words_most_common_number': 50,
         'pairs_count': pairs_count,
+        'pairs_most_common': pairs_most_common,
         'pairs_most_common_number': 50,
         'triplets_count': triplets_count,
+        'triplets_most_common': triplets_most_common,
         'triplets_most_common_number': 50,
     }
     output_directory = "./output/" + epub_title + "/"
     delta = datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)
-    timestamp = delta.total_seconds() * 1000
+    timestamp = int(delta.total_seconds() * 1000)
     output_filename = epub_title + str(timestamp)
 
     try:
@@ -55,12 +57,10 @@ def zipfs_law_analysis(epub_title: str):
     with open(output_directory + output_filename, 'w') as file:
         json.dump(json_output, file)
 
-    '''
     __generate_plots(words_count, words_ranked_data, 1, title="Words frequency", xlabel="log(rank)", ylabel="log(count)")
     __generate_plots(pairs_count, pairs_ranked_data, 2, title="Pairs frequency", xlabel="log(rank)", ylabel="log(count)")
     __generate_plots(triplets_count, triplets_ranked_data, 3, title="Triplets frequency", xlabel="log(rank)", ylabel="log(count)")
     plt.show()
-    '''
 
 
 def __get_word_pair_triple_count(epub_title: str):
@@ -78,13 +78,14 @@ def __get_word_pair_triple_count(epub_title: str):
         # Clean up anything else by escaping
         preprocessed_content = html.escape(no_tags).replace("\n", " ").strip()
         preprocessed_content = re.sub("\s\s+", " ", preprocessed_content)
+        # split = re.compile('^[a-zA-Z0-9\']+$').findall(preprocessed_content)
         if preprocessed_content:
             # print("CONTENT: ", preprocessed_content)
             nlp = spacy.load('en')
             doc = nlp(preprocessed_content)
             # [utils.to_nltk_tree(sentence.root).pretty_print() for sentence in doc.sents]
             for word in doc:
-                if word.pos_ not in ["PUNCT", "SYM", "X", "DET", "PART", "SPACE", "INTJ"]:
+                if word.pos_ in ["ADJ", "VERB", "CONJ", "DET", "NUM", "ADV", "ADP", "NOUN", "PROPN", "PRON"]:
                     words_list.append(word.text.lower())
             pairs_list = pairs_list + __extract_word_pairs(doc)
             triplets_list = triplets_list + __extract_word_triplets(doc)
@@ -162,5 +163,5 @@ def __generate_plots(counts_data, ranks_data, number=1, title="", xlabel="", yla
 
 
 def __get_top_range(ranked_data, limit=50):
-    sorted_data = sorted(ranked_data, key=lambda count: count)
+    sorted_data = sorted(ranked_data, reverse=True, key=lambda count: count[0][1])
     return sorted_data[0:limit]
